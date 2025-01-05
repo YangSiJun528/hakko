b8bfa0aef0f76efcde44f4082b31737dc04b38a6
sijun-yang <yangsijun5528@gmail.com>
2025-01-05 21:53:27 +0900

    chore

--- src/main/java/dev/joon/hakko/stream/LiveStream.java
+++ src/main/java/dev/joon/hakko/stream/LiveStream.java
@@ -10,5 +10,4 @@ public interface LiveStream {
     String getStreamingUrl();
     int getViewerCount();
     // TODO: 방송 시작시간 추가해서 최근 방송 시작한 사람을 제외하고 가져오기 (인기있는 사용자가 방송을 킨 직후는 해당되지 않게)
-    // CI 테스트용 주석
 }
